0556247f538f8ea19b88bab6024892dde40d002a
William Mansfield <rmansfield@realpeoplesratings.com>
2021-03-16 14:22:41 -0500

    Initial commit

---
+++ week-03/exercises/enum-exercises/src/test/java/learn/poker/TwoCardHandTest.java
@@ -0,0 +1,62 @@
+package learn.poker;
+
+import learn.cards.Card;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoCardHandTest {
+
+    // 1. Instantiate hands with the appropriate suit and rank for each test.
+    // 2. Run the tests and confirm they pass. Do NOT edit existing tests.
+    // 3. Add a couple more tests to confirm everything is working as intended.
+
+    @Test
+    void twoQueensShouldBeatTwo10s() {
+        // TODO: instantiate Cards and TwoCardHands with appropriate arguments
+        TwoCardHand queenQueen = new TwoCardHand(new Card(), new Card());
+        TwoCardHand tenTen = new TwoCardHand(new Card(), new Card());
+        assertTrue(queenQueen.compareTo(tenTen) > 0);
+    }
+
+    @Test
+    void two9sShouldBeatJack10() {
+        // TODO: instantiate Cards and TwoCardHands with appropriate arguments
+        TwoCardHand nineNine = new TwoCardHand(new Card(), new Card());
+        TwoCardHand jackTen = new TwoCardHand(new Card(), new Card());
+        assertTrue(nineNine.compareTo(jackTen) > 0);
+    }
+
+    @Test
+    void queen4ShouldBeatJack10() {
+        // TODO: instantiate Cards and TwoCardHands with appropriate arguments
+        TwoCardHand queenFour = new TwoCardHand(new Card(), new Card());
+        TwoCardHand jackTen = new TwoCardHand(new Card(), new Card());
+        assertTrue(queenFour.compareTo(jackTen) > 0);
+    }
+
+    @Test
+    void queen5ShouldBeatQueen3() {
+        // TODO: instantiate Cards and TwoCardHands with appropriate arguments
+        TwoCardHand queenFive = new TwoCardHand(new Card(), new Card());
+        TwoCardHand queenThree = new TwoCardHand(new Card(), new Card());
+        assertTrue(queenFive.compareTo(queenThree) > 0);
+    }
+
+    @Test
+    void two5sShouldTieTwo5s() {
+        // TODO: instantiate Cards and TwoCardHands with appropriate arguments
+        TwoCardHand firstFiveFive = new TwoCardHand(new Card(), new Card());
+        TwoCardHand secondFiveFive = new TwoCardHand(new Card(), new Card());
+        assertEquals(0, firstFiveFive.compareTo(secondFiveFive));
+    }
+
+    @Test
+    void jack4ShouldTieJack4() {
+        // TODO: instantiate Cards and TwoCardHands with appropriate arguments
+        TwoCardHand firstJackFour = new TwoCardHand(new Card(), new Card());
+        TwoCardHand secondJackFour = new TwoCardHand(new Card(), new Card());
+        assertEquals(0, firstJackFour.compareTo(secondJackFour));
+    }
+
+}

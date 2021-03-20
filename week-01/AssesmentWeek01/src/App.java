01b86bdc494e8402fd2fbd90ac00b6186082864e
Rick <rmansfield@realpeoplesratings.com>
2021-03-19 21:13:40 -0500

    Add readme and assessment

--- week-01/AssesmentWeek01/src/App.java
+++ week-01/AssesmentWeek01/src/App.java
@@ -1,117 +1,48 @@
 import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) {
-
-        // Your current location.
-        String location = "front";
-
-        // Must find these this item to win.
-        boolean hasKey = false;
-
-
-        // Receive commands.
         Scanner console = new Scanner(System.in);
-        String command = null;
 
-        while (!location.equals("done")) {
+        // step one display Welcome and prompt for capsules
+        int capsuleCount = AdminSetup(console);
+        String[] capsules = new String[capsuleCount];
 
-            switch (location) {
-                case "start":
-                    System.out.println("You wake up in a strange room, there are three doors around you.");
-                    System.out.println("Welcome to our breakout room, we want to be your advocate today. Try to look for hints as you go.");
-                    System.out.println("There\'s door locked door to the East.");
-                    System.out.println("There\'s a door to the West with a devil on it.");
-                    System.out.println("There\'s a door with an angel on it to your North.");
-                    System.out.println("Type the name of the image on the door to navigate: example \"angel\" , \"Devil\" , \"lockedDoor\" ");
-                    System.out.print("?: ");
-                    command = console.nextLine();
-                    if (command.equalsIgnoreCase("angel")) {
-                        location = "angel";
-                    } else if (command.equalsIgnoreCase("devil")) {
-                        location = "devil";
-                    } else if (command.equalsIgnoreCase("lockedDoor")) {
-                        if (!hasKey) {
-                            location = "lockedDoor";
-                        } else {
-                            System.out.println("You need a key to open this door");
-                            location = "start"; } //if they have key door unlocks
-                    } else {
-                        System.out.println("I don't understand that command.");
-                    }
-                    break;
-                case "angel":
-                    System.out.println("Bad choice the hint was \"Devils advocate\".");
-                    System.out.println("Your are in the North wing.");
-                    System.out.println("The door locks behind you");
-                    System.out.println("The angel of death sets the room on fire.");
-                    System.out.println("Game Over!");
-                    location = "start";
-                    break;
-                case "devil":
-                    System.out.println("Good choice the hint was Devils advocate.");
-                    System.out.println("The door locks behind you");
-                    System.out.println("Now you see a dragon door, and an elf door");
-                    System.out.println("Your journey is short.");
+        // step two initialize capsule hotel with user boundaries
+        createHotel(console, capsules);
 
-                    command = console.nextLine();
-
-                    if (command.equalsIgnoreCase("dragon")) {
-                        location = "angel";
-                    } else if (command.equalsIgnoreCase("elf")) {
-                        location = "elf";
-                    } else {
-                        System.out.println("I don't understand that command.");
-                    }
-                    break;
-
-                case "lockedDoor":
-                    System.out.println("Victor! You win");
-                    location = "start";
-                    break;
-
-                case "elf":
-                    System.out.println("Good choice, you passed safely to this room.");
-                    System.out.println("The door locks behind you.");
-                    System.out.println("You see two final doors in front of you,\n one labeled One and the other Zero");
-                    System.out.println("Life is binary.");
-                    System.out.print("?: ");
-                    command = console.nextLine();
-
-                    if (command.equalsIgnoreCase("zero")) {
-                        location = "angel";
-                    } else if (command.equalsIgnoreCase("one")) {
-                        location = "one";
-                    } else {
-                        System.out.println("I don't understand that command.");
-                    }
-                    break;
-
-                case "one":
-                    System.out.println("You enter the final room and see a key hanging from the ceiling.");
-                    System.out.println("Do you take it Yes/No: ");
-
-                    command = console.nextLine();
-
-                    if (command.equalsIgnoreCase("yes")) {
-                        hasKey = true;
-                        System.out.println("You got the master-key for the all the doors. You win!");
-                        location = "start";
-                    } else if (command.equalsIgnoreCase("no")) {
-                        System.out.println("But why? Now you die.");
-                        location = "angel";
-                    } else {
-                        System.out.println("I don't understand that command.");
-                    }
-                    location = "done";
-                    break;
-
-                default:
-                    // System.out.println("-default bug tool-");
-                    location = "start";
-
-            }
-
-        }
     }
+
+    //method for the initial set up
+    private static int AdminSetup(Scanner console) {
+        System.out.println("Welcome to the Capsule Hotel App!");
+        System.out.println("<===============================>");
+        System.out.print("Please enter the capsules in your hotel: ");
+
+        int capsuleCount = Integer.parseInt(console.next());
+
+        System.out.println("Excellent, you have " + capsuleCount + " available capsules for occupancy.");
+        return capsuleCount;
+    }
+
+    //method for the initial hotel creation
+    private static void createHotel(Scanner console, String[] capsules) {
+        boolean exitApp = false;
+        DisplayMenu();
+    }
+    // method for creating/recreating the main menu to display
+    private static void DisplayMenu() {
+        System.out.println("\nGuest Menu");
+        System.out.println("<==========>");
+        System.out.println("1. Check In");
+        System.out.println("2. Check  out");
+        System.out.println("3. View Guests");
+        System.out.println("4. Exit");
+        System.out.print("Choose an option [1-4]");
+    }
+
+
+
+
 }
---
+++ week-01/AssesmentWeek01/src/README.md
@@ -0,0 +1,54 @@
+## Pre Start Set up
+* [x] instantiate scanner to receive commands
+
+## Start up
+* [x] prompt for number of capsules/capacity int capacity = user input
+    *  [ ] Generate capsules array
+       it will be capsules[capcity]
+        * array contains number of unoccupied capsules user wants
+* [ ] Display confirmation message
+    * "There are X unoccupied capsules..."
+
+## case Main Menu
+* [ ] Display main Guest Menu (see given example above)
+    * [ ] initiate main while loop for navigation
+    * Use Switch (Admin Action)
+        * Case 1. CheckIn
+        * Case 2. CheckOut
+        * Case 3. View Guests
+        * Case 4. Exit
+* [ ] console for collecting user's menu choice
+    * uses do/while for defensive validation
+* [ ] choice displays/redirects to appropriate menu choice
+
+## 1. Check In
+* [ ] print a check in header
+*  [ ] prompt for Guest Name & Accept in line
+*  [ ] prompt for room number choice
+    *   [ ] store name in array at the index of room number
+    *   [ ] Check occupancy
+        * occupied display error and re-prompt
+        * capsules[unoccupied changed to user number choice] = guest name
+* Once successful, print success message and go back to main menu
+
+## 2. Check Guest Out
+* [ ] print a check out header
+* [ ] prompt for capsule collect in line answer
+* [ ] Check occupancy
+    * unoccupied display error "not occupied"
+    * if occupied ... remove name from capsule by replacing with "unoccupied"
+        * display success method and go back to main menu
+
+## 3. View Guests
+* The administrator may view guests and their capsule numbers in groups of 10.
+* must keep user in bounds of number of capsules.
+* prompt for capsule # (also in line again)
+* use that number to display 10 additional capsule and guest conditions
+    * basically print the array with these 11 indexes from the capsules array
+
+## 4. Exit
+* print exit header
+* confirm this is not a mistake
+    * Exit y/n
+    * if yes goodbye!
+    * if no back to main menu 

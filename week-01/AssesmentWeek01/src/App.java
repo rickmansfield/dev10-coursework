b4f90bd40ce7313a115d36687853d82f04e88041
Rick <rmansfield@realpeoplesratings.com>
2021-03-20 08:12:56 -0500

    Added guest check in method

--- week-01/AssesmentWeek01/src/App.java
+++ week-01/AssesmentWeek01/src/App.java
@@ -12,6 +12,8 @@ public class App {
         // step two initialize capsule hotel with user boundaries
         createHotel(console, capsules);
 
+        int adminChoice = Integer.parseInt(console.next());
+
     }
 
     //method for the initial set up
@@ -31,6 +33,7 @@ public class App {
         boolean exitApp = false;
         DisplayMenu();
     }
+
     // method for creating/recreating the main menu to display
     private static void DisplayMenu() {
         System.out.println("\nGuest Menu");
@@ -41,9 +44,6 @@ public class App {
         System.out.println("4. Exit");
         System.out.print("Choose an option [1-4]");
     }
-    //While Loop Here?
-    int adminChoice =
-
 
     //method for guest check in
     private static void CheckIn(Scanner console, String[] capsules) {
@@ -51,13 +51,33 @@ public class App {
         System.out.println("<=================>");
         System.out.print("Guest Name: ");
         String guestName = console.next();
+
+        boolean checkedIn = false;
+        do {
+            System.out.print("Capsule #[1- " + capsules.length + "]: ");
+            int capsuleNumber = Integer.parseInt(console.next());
+
+            if (capsuleNumber <1 || capsuleNumber > capsules.length) {
+                System.out.println("Sorry, that is an invalid capsule, please select between Capsule numbers #[1- " + capsules.length + "]: ");
+                continue;
+            }
+
+            if (capsules[capsuleNumber -1] == null) {
+                capsules[capsuleNumber -1] = guestName;
+                System.out.println("\n" + guestName + "successfully booked in capsule #" + capsuleNumber);
+                checkedIn = true;
+            } else {
+                System.out.println("\nTry again. Capsule #" + capsuleNumber + " is occupied.");
+                //checkedIn = false; //not sure if I have to reaffirm the checked in status.
+            }
+        } while (!checkedIn);
      }
 
     //method for guest check out
-    //private static void CheckOut(Scanner console, String[] capsules) {}
+    private static void CheckOut(Scanner console, String[] capsules) {}
 
     //method for exit
-    //private static void Exit(Scanner console){}
+    private static void Exit(Scanner console){}
 
 
 

d97c7b0221d1ea3cb5eb0bd70b35a44eb424dd52
Rick <rmansfield@realpeoplesratings.com>
2021-03-24 21:15:35 -0500

    Implement CyclistTest class

--- week-02/m2/learn-utl/src/main/java/learn/testing/ArrayMethods.java
+++ week-02/m2/learn-utl/src/main/java/learn/testing/ArrayMethods.java
@@ -1,4 +1,40 @@
 package learn.testing;
 
 public class ArrayMethods {
+    public static int[] shiftLeft(int[] values) {
+        //OLD CODE
+//        if (values == null) { // account for null
+//            return null;
+//        }
+//
+//        if (values.length <= 1) { // nothing to shift
+//            return values;
+//        }
+//
+//        int element = values[0];
+//        for (int i = 1; i < values.length; i++) {
+//            values[i - 1] = values[i];
+//        }
+//        values[values.length - 1] = element;
+//        return values;
+
+        //NEW CODE
+        if (values == null) { // account for null
+            return null;
+        }
+
+        int[] result = new int[values.length];
+        if (result.length == 0) { // account for empty array
+            return result;
+        }
+
+        for (int i = 1; i < values.length; i++) {
+            result[i - 1] = values[i];
+        }
+        result[result.length - 1] = values[0];
+
+        return result;
+    }
+
+
 }

1d1883a853cc7b32cf9e5b7b30c8aff6c6db0127
Rick <rmansfield@realpeoplesratings.com>
2021-03-25 23:09:52 -0500

    update Stateless Exercise 04

--- week-02/exercises/stateless-unit-test-exercises/src/main/java/Exercise04.java
+++ week-02/exercises/stateless-unit-test-exercises/src/main/java/Exercise04.java
@@ -24,25 +24,35 @@ public class Exercise04 {
      */
     public double calculateTotalCost(double price, int quantity) {
         int discountCase = 0;
-        if (quantity >= 1 && quantity <= 15) {switchCase = 1;}
-        if (quantity >= 16 && quantity <= 25) {switchCase = 2;}
-        if (quantity >= 26 && quantity <= 50) {switchCase = 3;}
-        if (quantity >= 51 && quantity <= 74) {switchCase = 4;}
-        if (quantity >= 76) {switchCase = 5;}
-        switch(discountCase){
+        if (quantity >= 1 && quantity <= 15) {
+            switchCase = 1;
+        }
+        if (quantity >= 16 && quantity <= 25) {
+            switchCase = 2;
+        }
+        if (quantity >= 26 && quantity <= 50) {
+            switchCase = 3;
+        }
+        if (quantity >= 51 && quantity <= 74) {
+            switchCase = 4;
+        }
+        if (quantity >= 76) {
+            switchCase = 5;
+        }
+        switch (discountCase) {
             case 1:
-                return price * quantity;
+                cost = price * quantity;
             case 2:
-                return (price * quantity) * 0.95;
+                cost = (price * quantity) * 0.95;
             case 3:
-                return (price * quantity) * 0.90;
+                cost = (price * quantity) * 0.90;
             case 4:
-                return (price * quantity) * 0.85;
+                cost = (price * quantity) * 0.85;
             case 5:
-                return (price * quantity) * 0.75;
+                cost(price * quantity) * 0.75;
 
         }
-        return price;
+        return cost;
     }
 
 }

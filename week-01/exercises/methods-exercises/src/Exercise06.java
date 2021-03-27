ea5571a03f1eac4829f0c3981b99b36816554481
Rick <rmansfield@realpeoplesratings.com>
2021-03-27 11:56:07 -0500

    Update Methods Exercise06

--- week-01/exercises/methods-exercises/src/Exercise06.java
+++ week-01/exercises/methods-exercises/src/Exercise06.java
@@ -9,5 +9,16 @@ public class Exercise06 {
 
     public static void main(String[] args) {
         // 2. Call your method in various ways to test it here.
+        System.out.println(isBetween(2,1, 10));//true
+        System.out.println(isBetween(5,1, 10));//true
+        System.out.println(isBetween(0,1, 10));//false
+        System.out.println(isBetween(1,5, 10));//false
+
+    }
+
+    public static boolean isBetween(int number, int minValueInclusive, int maxValueInclusive){
+        if (number >= minValueInclusive && number <= maxValueInclusive){
+            return true;
+        } else { return false;}
     }
 }
